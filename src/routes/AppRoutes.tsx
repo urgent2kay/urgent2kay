00750b8a3688b81400b83ea990fa81aae09ad3c5
Mariolxxy <gracemario164@gmail.com>
2025-04-30 12:07:56 +0100

    created signup page

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import GenerateRequest from "../pages/GenerateRequest";
 import TemplatePage from "../pages/Template";
 import BundleOverview from "../pages/BundleOverview";
+import Signup from "../pages/Signup";
 
 const AppRoutes: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ const AppRoutes: React.FC = () => {
       <Route path="/generate-request" element={<GenerateRequest />} />
       <Route path="/template" element={<TemplatePage />} />
       <Route path="/bundle-overview" element={<BundleOverview />} />
+      <Route path="/sign-up" element={<Signup />} />
     </Routes>
   );
 };
