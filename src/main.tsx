a06efd114838295527a2cbf8290a20480aea60f9
YourGitHubUsername <your_email@example.com>
2025-05-16 16:08:19 +0100

    update landing page

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import App from './App';
 import { Provider } from 'react-redux';
-import { store } from "./store/store";
-
-
-
+import { store } from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
